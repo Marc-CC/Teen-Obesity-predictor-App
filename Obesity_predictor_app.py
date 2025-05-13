28cc9895d50e06c3cd910db11f36ca60291cd2e8
MaCeKa <marccanete99@gmail.com>
2025-05-13 23:43:24 +0200

    App developed - Works

---
+++ Obesity_predictor_app.py
@@ -0,0 +1,122 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import subprocess
+import json
+from tensorflow.keras.models import load_model
+
+
+st.title("_Teen Obesity predictor app_")
+
+st.write("This app uses a pre-trained machine learning model to predict the zbmi score and its label of children between the ages 2 through 13 at 14 years old." \
+"Please take into consideration that the prediction might not be accurate and should always be reviewed by an expert (i.e. pediatrician).")
+
+st.header("Data input form", divider = "grey")
+
+st.subheader("Enter available Height (cm) and Weight (kg) data between ages 2 through 13.")
+
+if "child_info" not in st.session_state:
+    st.session_state.child_info = pd.DataFrame(columns=["Age", "Height_cm", "Weight_kg"])
+
+# Create form
+if "child_sex" not in st.session_state:
+    st.session_state.child_sex = pd.DataFrame(columns=["Sex"])
+
+# Form for one age only per submission
+with st.form(key="child_form"):
+    sex = st.selectbox("Enter child sex:", ["Male", "Female"])
+
+    submitted = st.form_submit_button("Submit Entry")
+
+    if submitted:
+        new_row = pd.DataFrame({
+            "Sex": [sex] * 12
+            })
+        st.session_state.child_sex = pd.concat([st.session_state.child_sex, new_row], ignore_index=True)
+        st.success(f"Entry added")
+
+# Create st.dataframe
+Age = list(range(2, 14))
+Height_cm = [None] * len(Age)
+Weight_kg = [None] * len(Age)
+
+child_df = pd.DataFrame({"Age": Age,
+                       "Height_cm": Height_cm,
+                       "Weight_kg": Weight_kg
+                       })
+
+edited_child_df = st.data_editor(child_df,
+                               use_container_width=True,
+                               height=457,
+                               column_config={
+                                   "Age": st.column_config.NumberColumn(disabled=True)
+                                   })
+
+# Convert df types
+edited_child_df["Age"] = edited_child_df["Age"].astype(int)
+edited_child_df["Height_cm"] = edited_child_df["Height_cm"].astype(float)
+edited_child_df["Weight_kg"] = edited_child_df["Weight_kg"].astype(float)
+
+# Fill child_info with edited data
+st.session_state.child_info = edited_child_df.copy()
+
+# Add Download button and combine data into single df
+combined_data = pd.concat([st.session_state.child_sex, st.session_state.child_info], axis=1)
+csv = combined_data.to_csv(index=False)
+st.write("Download child data as CSV file (optional to keep your data)")
+st.download_button("Download", data=csv, file_name="child_data.csv", mime="text/csv")
+
+st.header("Zbmi Prediction at 14 years old")
+if st.button("Predict"):
+    json_data = combined_data.to_json(orient="records")
+    
+    with open("child_data.json", "w") as f:
+        f.write(json_data)
+
+    subprocess.run(["Rscript", "Subprocess.R"])
+
+    with open("child_data_processed.json", "r") as f:
+        processed_data = json.load(f)
+
+    processed_data = pd.DataFrame(processed_data)
+
+    model = load_model("GRU_obesity_zbmi_predictor")
+
+    # Process into array
+    def preprocess_to_3d_array(df, measure_cols, sex_cols, real_col):
+        measures = df[measure_cols].values.reshape(-1, len(measure_cols), 1)
+        sex = df[sex_cols].values
+        sex_repeated = np.repeat(sex[:, np.newaxis, :], len(measure_cols), axis=1)
+        is_real_value = df[real_col].apply(lambda x: 0 if x == "Unknown" else 1).values
+        is_real_value = is_real_value.reshape(-1, 1, 1)
+        is_real_value = np.repeat(is_real_value, len(measure_cols), axis=1)
+        Combined_array = np.concatenate([measures, sex_repeated, is_real_value], axis=2)
+        return Combined_array
+    
+    # Define a converter function
+    def convert_to_label(predictions):
+        labels = []
+        for prediction in predictions:
+            match prediction:
+                case _ if prediction < -2.0:
+                    labels.append("Underweight")
+                case _ if -2.0 <= prediction <= 1.0:
+                    labels.append("Normal_weight")
+                case _ if 1.0 < prediction <= 2.0:
+                    labels.append("Overweight")
+                case _ if prediction > 2.0:
+                    labels.append("Obese")
+                case _:
+                    labels.append(np.nan)
+        return labels
+
+    Measure_cols_3d_array = [f"zbmi_{i}" for i in range(2, 14)]
+    processed_data_3d = preprocess_to_3d_array(processed_data, Measure_cols_3d_array, ["sex_Female", "sex_Male"], "stratify")
+    processed_data_3d = np.where(processed_data_3d == None, np.nan, processed_data_3d).astype(float)
+    processed_data_3d = np.nan_to_num(processed_data_3d, nan=999.0)
+
+    zbmi_prediction = model.predict(processed_data_3d)
+    zbmi_label = convert_to_label(zbmi_prediction)
+
+    st.write("Zbmi prediction: ", zbmi_prediction[0][0])
+    st.write("Label predicted: ", zbmi_label)
---
+++ Subprocess.R
@@ -0,0 +1,110 @@
+# Subprocess for streamlit app ----
+library(jsonlite)
+library(growthcleanr)
+library(anthro)
+library(anthroplus)
+library(tidyverse)
+
+# Load inputs ----
+input <- fromJSON("child_data.json")
+
+# Re-check and convert types ----
+input$Sex = as.character(input$Sex)
+input$Age = as.numeric(input$Age)
+input$Height_cm = as.numeric(input$Height_cm)
+input$Weight_kg = as.numeric(input$Weight_kg)
+
+# Processing ----
+
+## Growthcleanr ----
+
+### Prepare for growthcleanr ----
+
+# Split into two dfs
+height_data <- input[, !colnames(input) %in% "Weight_kg"]
+weight_data <- input[, !colnames(input) %in% "Height_cm"]
+
+height_data$param <- rep("HEIGHTCM", dim(height_data)[1])
+weight_data$param <- rep("WEIGHTKG", dim(weight_data)[1])
+
+colnames(height_data) <- c("Sex", "Age", "measurement", "param")
+colnames(weight_data) <- c("Sex", "Age", "measurement", "param")
+
+# Re-fuse dfs and finish preparing data
+growthclean <- rbind(height_data, weight_data)
+growthclean$Age <- growthclean$Age * 365.25
+growthclean$Sex <- ifelse(growthclean$Sex == "Female", "F", "M")
+growthclean$subjid <- rep("subjid", dim(growthclean)[1])
+
+### Call cleangrowth
+gcr_result <- cleangrowth(subjid = growthclean$subjid,
+                          param = growthclean$param,
+                          agedays = growthclean$Age,
+                          sex = growthclean$Sex,
+                          measurement = growthclean$measurement)
+growthclean <- cbind(growthclean, gcr_result)
+
+# Zbmi calculation ----
+
+## Data preparation ----
+growthclean <- growthclean[growthclean$gcr_result == "Include", ]
+growthclean <- growthclean[, !colnames(growthclean) %in% "gcr_result"]
+
+growthclean <- growthclean %>%
+  pivot_wider(names_from = param, values_from = measurement) %>%
+  filter(!is.na(HEIGHTCM) & !is.na(WEIGHTKG))
+
+growthclean$Sex <- ifelse(growthclean$Sex == "F", 1, 0)
+
+# Split by anthro and anthroplus ages
+anthro <- growthclean[growthclean$Age < (5 * 365.25), ]
+anthroplus <- growthclean[growthclean$Age >= (5 * 365.25), ]
+original_age_anthroplus <- anthroplus$Age
+anthroplus$Age <- anthroplus$Age / 30.4375
+
+anthro[["zbmi"]] <- with(
+  anthro,
+  anthro_zscores(
+    sex = Sex, age = Age,
+    weight = WEIGHTKG, lenhei = HEIGHTCM)
+  ) %>% pull(zbmi)
+
+anthroplus[["zbmi"]] <- with(
+  anthroplus,
+  anthroplus_zscores(
+    sex = Sex, age = Age,
+    weight_in_kg = WEIGHTKG, height_in_cm = HEIGHTCM)
+) %>% pull(zbfa)
+
+anthroplus$Age <- original_age_anthroplus
+
+# Merge all data ----
+All <- bind_rows(anthro, anthroplus) %>% 
+  select(Sex, Age, zbmi) %>%
+  mutate(Age = Age / 365.25)
+
+# Prepare for model ----
+All <- pivot_wider(All, names_from = Age,
+                   values_from = zbmi,
+                   names_glue = "zbmi_{Age}")
+
+ages <- seq(2, 13, 1)
+zbmi_cols <- paste0("zbmi_", ages)
+other_cols <- setdiff(names(All), zbmi_cols)
+
+for (col in zbmi_cols) {
+  if (!col %in% names(All)) {
+    All[[col]] <- NA
+  }
+}
+
+All <- All[, c(zbmi_cols, other_cols)]
+
+All$sex_Female <- ifelse(All$Sex == 1, 1.0, 0.0)
+All$sex_Male <- ifelse(All$Sex == 1, 0.0, 1.0)
+All$Sex <- NULL
+
+All$stratify <- 0
+
+# Export ----
+write(toJSON(All, auto_unbox = TRUE, na = "null"), "child_data_processed.json")
